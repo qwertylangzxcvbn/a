86ff6409f00dee466aef9ae96eca9dfa623272f6
YourKohai <sh.tema@icloud.com>
2022-09-11 19:16:13 +0300

    Revert "Revert "a2""
    This reverts commit b088463a80a091a09135344adf06638236dfc534.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using system;
+namespace ConsoleApp1
+{ 
+    iternal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ab;
+        }
+    }
+}
